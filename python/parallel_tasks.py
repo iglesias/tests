ca7c76b83a83c11d9209d01afe3b0b647e04e902
Fernando Iglesias <fernando.iglesiasg@gmail.com>
2013-08-25 01:30:30 +0200

    Minimal Python script to execute several tasks in parallel using the subprocess module.

---
+++ python/parallel_tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from subprocess import Popen
+
+commands = [
+	'date; ls -l; sleep 1; date',
+	'date; sleep 5; date',
+	'date; df -h; sleep 3; date',
+	'date; hostname; sleep 2; date',
+	'date; uname -a; date',
+]
+# run in parallel
+processes = [Popen(cmd, shell=True) for cmd in commands]
+# do other things here...
+# wait for completion
+for p in processes: p.wait()
