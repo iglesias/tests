47fb634c1a0ac8c00afa653353f68a782cebc82d
Fernando Iglesias <fernando.iglesiasg@gmail.com>
2013-08-26 12:47:44 +0200

    Update diagonal example storing intermediate diagonal result in a vector.

--- c++/eigen/Diagonal.cc
+++ c++/eigen/Diagonal.cc
@@ -6,9 +6,9 @@ int main(int,char**) {
 	A << 1,2,3,4;
 
 	std::cout << "Initial matrix:\n" << A << std::endl;
-	std::cout << "Its main diagonal:\n" << A.diagonal() << std::endl;
- 	std::cout << "Its main diagonal as a diagonal matrix:\n" <<
-			Eigen::Matrix2i(A.diagonal().asDiagonal()) << std::endl;
+	Eigen::Vector2i a = A.diagonal();
+	std::cout << "Its main diagonal:\n" << a << std::endl;
+	std::cout << "Its main diagonal as a diagonal matrix:\n" << Eigen::Matrix2i(a.asDiagonal()) << std::endl;
 
 	return 0;
 }
