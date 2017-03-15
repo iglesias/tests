421d974c1969841c21719868cc89ad400bd9a468
Fernando Iglesias Garcia <f.iglesias@scisports.com>
2017-03-15 16:25:46 +0100

    Toy cuda program using constant memory and cudaMemcpyToSymbol.
    https://stackoverflow.com/questions/42813955/invalid-device-symbol-error-depending-on-nvcc-flags

---
+++ c++/cuda/cuda_invalid_symbol_error.cu
@@ -0,0 +1,34 @@
+/* 
+ * cuda_innvalid_symbol_error.cu
+ *
+ * Compilation triggering runtime error:
+ *  nvcc -gencode arch=compute_52,code=sm_52 -std=c++11 cuda_invalid_symbol_error.cu -o cuda_invalid_symbol_error
+ *
+ * Error-free compilation:
+ *  nvcc -arch=sm_52 -std=c++11 cuda_invalid_symbol_error.cu -o cuda_invalid_symbol_error
+ */ 
+
+#include <iostream>
+#include <vector>
+
+// Matrix side size (they are square).
+const int N = 3;
+const int num_mats = 14;
+
+// Rotation matrices.
+__constant__ float rot_mats_device[num_mats*N*N];
+
+int main() {
+  std::vector<float> rot_mats_host(num_mats*N*N);
+  for (int i = 0; i < rot_mats_host.size(); i++)
+    rot_mats_host[i] = i;
+
+  auto errMemcpyToSymbol = cudaMemcpyToSymbol(rot_mats_device,
+                                              rot_mats_host.data(),
+                                              sizeof(rot_mats_device));
+
+  if (errMemcpyToSymbol != cudaSuccess) {
+    std::cout << "MemcpyToSymbol error: " <<
+      cudaGetErrorString(errMemcpyToSymbol) << std::endl;
+  }
+}
