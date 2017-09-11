1da2b15a2d9434b3c72dab9cd0c7ac3a41a16239
Fernando Iglesias Garcia <f.iglesias@scisports.com>
2017-09-11 13:50:11 +0200

    Update example using stl's Deque.
    - Reindent.
    - Print types for fun.

--- c++/Deque.cc
+++ c++/Deque.cc
@@ -1,29 +1,26 @@
-#include <deque>
-#include <iostream>
 #include <cstdio>
+#include <deque>
+#include <typeinfo>
 
-int main(int, char**)
-{
-	std::deque<int> int_deque;
-	for (int i = 0; i < 10; ++i) {
-		if (i % 2 == 0)
-			int_deque.push_front(i);
-		else
-			int_deque.push_back(i);
-	}
+int main() {
+  std::deque<int> int_deque;
+  printf("%s %s\n", typeid(int_deque).name(), typeid(std::deque<int>).name());
+  for (int i = 0; i < 10; ++i) {
+    if (i % 2 == 0)
+      int_deque.push_front(i);
+    else
+      int_deque.push_back(i);
+  }
 
-	while (!int_deque.empty()) {
-		printf("%d%c", int_deque.front(), int_deque.size() == 1 ? '\0': ' ');
-		int_deque.pop_front();
-	}
-	printf("\n");
+  while (!int_deque.empty()) {
+    printf("%d%c", int_deque.front(), int_deque.size() == 1 ? '\0' : ' ');
+    int_deque.pop_front();
+  }
+  printf("\n");
 
-	for (int i = 0; i < 10; ++i)
-		int_deque.push_back(i);
+  for (int i = 0; i < 10; ++i) int_deque.push_back(i);
 
-	for (auto it = int_deque.begin(); it != int_deque.end(); ++it)
-		printf("%d ", *it);
-	printf("\n");
-
-	return 0;
+  for (auto it = int_deque.begin(); it != int_deque.end(); ++it)
+    printf("%d ", *it);
+  printf("\n");
 }
